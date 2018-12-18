287131f7cca4bf8442f3f97bfdc34f971ed28011
Steve <steven.bulmer@qut.edu.au>
2018-12-18 11:04:06 +1000

    test code done

--- on_robot/readme.md
+++ on_robot/readme.md
@@ -1,3 +1,4 @@
 # In this folder
-1. code to collect training data 
-2. example code to deploy your training model
+1. run test_camera_motors.py to test your robot
+2. code to collect training data 
+3. example code to deploy your training model
--- on_robot/test_camera_motors.py
+++ on_robot/test_camera_motors.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+import deploy.penguinPi as ppi
+import time
+
+if __name__ == "__main__":
+    print("test left motor")
+    ppi.set_velocity(10,0)
+    time.sleep(3)
+    print("test right motor")
+    ppi.set_velocity(0,10)
+    time.sleep(3)
+    print("stop")
+    ppi.set_velocity(0,0)
+    print("capture image")
+    image = ppi.get_image()
+    print("image size %d by %d" % (image.shape[0],image.shape[1]))
+
